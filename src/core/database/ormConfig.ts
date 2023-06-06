4bd09a0a1bf638968a2a07a0ca7a06e5cd0dd281
miguel_jardinez <jardinez.ramos.miguel@gmail.com>
2023-06-06 14:24:21 -0500

    feat: update testing files

--- src/core/database/ormConfig.ts
+++ src/core/database/ormConfig.ts
@@ -4,14 +4,25 @@ import { DataSourceOptions } from 'typeorm';
 const isDevelop = process.env.NODE_ENV === 'development';
 const isProduction = process.env.NODE_ENV === 'production';
 
+/**
+ * Information for database
+ * configurations
+ */
+
+const DB_PASSWORD = process.env.PGPASSWORD;
+const DB_USER = process.env.PGUSER;
+const DB_HOST = process.env.PGHOST;
+const DB_DATABASE = process.env.PGDATABASE;
+const DB_PORT = process.env.PGPORT;
+
 export const dbConfig = {
   type: 'postgres',
   url: process.env.POSTGRES_URL,
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'amazon_clone_db',
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   entities: [`${__dirname}/../../**/*.entity.{ts,js}`],
   synchronize: isDevelop,
   logging: true,
